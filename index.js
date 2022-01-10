b8596d907dae8e157e9e67c475d3161ddec2252a
Gil <gcastillohtx@gmail.com>
2022-01-10 11:13:13 -0600

    commit tree 30f4b94032d07bf711bdd89601efb75185db72b3

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let multiply = (num1 * num2)
+//////
+let random = Math.floor(Math.random() * 100) + 1
+////
+let num3 = 19 
+let num4 = 5
+let mod = (num3 % num4)
+////
+let max = Math.max(5, 10, 15, 17, 20)
 
-// Write your code here
